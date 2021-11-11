acd12ea021c6d374ea6b04193889cc068b36eb06
Andrey Bondar <andrey.bondar.2003@list.ru>
2021-11-11 11:47:25 +0300

    Пара улучшений, включая улучшения рандома. Плюс привел таблицу эффективности

--- Genetic_algorithm.cpp
+++ Genetic_algorithm.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 long double function(double arr[3])
 {
-    return 11/(1+pow(arr[0]-858.04, 2)) + 1/(1+pow(arr[1]+180.47, 2)) + 1/(1+pow(arr[2]+153.887, 2));
+    /*return pow(arr[1],4) + 2 * arr[0] *arr[1] + pow(arr[0], 2) + 2*arr[1] + pow(arr[1],2);*/
+    return -pow(arr[0],2) - 5 * pow(arr[1],2) - 3 * pow(arr[2], 2) + arr[0] * arr[1] - 2 * arr[0] * arr[2] + 2 * arr[1] * arr[2] + 11 * arr[0] + 2 * arr[1] + 18 * arr[2] + 10;
+    return 11/(1+pow(arr[0]-8.04, 2)) + 1/(1+pow(arr[1]+0.455557, 2)) + 1/(1+pow(arr[2]+10.1087, 2));
     return pow(arr[2], 3) - pow(arr[0], 2) + 3 * pow(arr[1], 2) - 1.5 * pow(arr[2], 2) - 4 * arr[0] + 6 * arr[1] + 2;
 }
 
 double random()
 {
-    int n = 4;
+    int n = 6;
+    int n1 = 4;
     /*return rand() % (int)pow(10, rand() % 4 + 1);*/
-    return (double)((int)pow(rand(), 2) % (int)pow(10, (rand() % n + 1))) * (pow(10, -(n - 1) + (rand() % (n - 1) + 1)));
-
+    return (double)((int)pow(rand(), 2) % (int)pow(10, (rand() % n + 1))) * (pow(10, -(n1) + (rand() % (n1) + 1)));
 }
 
 void sorted(long double* arr0, double arr1[][3], int LEN, int k = 1)
@@ -38,13 +40,13 @@ void sorted(long double* arr0, double arr1[][3], int LEN, int k = 1)
 
 void crossing(double arr[][3], int k[3], int k_parence, int p, int LEN, int q = 1)
 {
-    if (0) {
+    if (rand()%3) {
         for (int u = 0; u < 3; u++) {
             for (int i = 0; i < k_parence; i++) {
                 for (int j = i + 1; j < k_parence; j++) {
                     if (p >= rand() % 100 + 1) {
                         double a = -0.25 + (double)(rand() % 125) / 100.0;
-                        /*arr[k[u]][u] = (arr[i][u] + arr[j][u]) / 2;*/
+                        //arr[k[u]][u] = (arr[i][u] + arr[j][u]) / 2;  // среднее арифметическое
                         arr[k[u]][u] = arr[i][u] + a * (arr[j][u] - arr[i][u]);  // Промежуточная рекомбинация (Intermediate recombination)
                         k[u]--;
                     }
@@ -80,7 +82,7 @@ void crossing(double arr[][3], int k[3], int k_parence, int p, int LEN, int q = 
 void mutation(double arr[][3], int k[3], int k_parence, int p)
 {
     for (int i = 0; i < k_parence; i++)  // Real valued mutation
-        if (rand() % 2) {
+        if (rand()%3) {
             double eps = pow(10, -2 + rand() % 4);
             for (int j = 0; j < 3; j++) {
                 arr[k[j]][j] = arr[i][j] + eps;
@@ -114,41 +116,65 @@ void mutation(double arr[][3], int k[3], int k_parence, int p)
     }
 }
 
-bool check_stop(double arr1[][3], double arr2[][3], int LEN)
+bool check_stop(long double arr1[], long double arr2[], int LEN)
 {
-    int x = 10;
     int k[2][3] = { 0 };
     for (int j = 0; j < 3; j++) {
-        for (int i = 1; i < LEN; i++) {
-            if (arr1[i][j] + x > arr1[0][j])
+        for (int i = 1; i < LEN*0.45; i++) {  // a == b
+            if (arr1[i] == arr1[0])
                 k[0][j]++;
-        }
-    }
-    if ((k[0][0] > (int)LEN * 0.5) && (k[0][1] > (int)LEN * 0.5) && (k[0][2] > (int)LEN * 0.5))
-        return false;
-    for (int j = 0; j < 3; j++) {
-        for (int i = 1; i < LEN; i++) {
-            if (arr2[i][j] + x > arr2[0][j])
+            if (arr2[i] == arr2[0])
                 k[1][j]++;
         }
+        if (k[0][j] >= 40)
+            return false;
+        if (k[1][j] >= 40)
+            return false;
     }
-    if ((k[1][0] > (int)LEN * 0.5) && (k[1][1] > (int)LEN * 0.5) && (k[1][2] > (int)LEN * 0.5))
-        return false;
     return true;
 }
 
 int main() // функция номер 20
 {
     setlocale(LC_ALL, "rus");
+    system("chcp 1251");
+    system("cls");
 
-    /*double eps = 0.1;*/
     double probability_m = 60, probability_c = 80;
     const int LEN = 100;
+    int iter_max = 1000;
     int k_parence = 10, iter = 0;
     int k_individuals_max[3], k_individuals_min[3];
 
-    /*cout << "Введите точность: " << endl;
-    cin >> eps;*/
+    int q;
+    /*cout << "Будете вводить переменные(1-да): " << endl;
+    cin >> q;*/
+    q = 0;
+    switch (q)
+    {
+    case 1:
+        cout << "Введите вероятность скрещивания (рекомбинации) [0 - 100]: " << endl;
+        cin >> probability_c;
+        if (probability_c < 0 || probability_c > 100) {
+            cout << "Нкорректные данные" << endl;
+            return 0;
+        }
+        cout << "Введите вероятность мутации [0 - 100]: " << endl;
+        cin >> probability_m;
+        if (probability_m < 0 || probability_m > 100) {
+            cout << "Нкорректные данные" << endl;
+            return 0;
+        }
+        cout << "Введите максимальное количество итераций [>0]: " << endl;
+        cin >> iter_max;
+        if (iter_max < 0) {
+            cout << "Нкорректные данные" << endl;
+            return 0;
+        }
+        break;
+    default:
+        break;
+    }
 
     double population_max[LEN][3], population_min[LEN][3];
     long double func_min[LEN], func_max[LEN];
@@ -170,7 +196,7 @@ int main() // функция номер 20
         k_individuals_max[0] = k_individuals_max[1] = k_individuals_max[2] = LEN - 1;
         k_individuals_min[0] = k_individuals_min[1] = k_individuals_min[2] = LEN - 1;
 
-        crossing(population_max, k_individuals_max, k_parence, probability_c, LEN);   // Нахождение следующего поколения (среднее арифметическое)
+        crossing(population_max, k_individuals_max, k_parence, probability_c, LEN);   // Нахождение и скрещивание следующего поколения
         mutation(population_max, k_individuals_max, k_parence, probability_m);  // Нахождение окрестности + Заполнение остатка массива случайными числами
 
         crossing(population_min, k_individuals_min, k_parence, probability_c, LEN, -1);
@@ -192,9 +218,9 @@ int main() // функция номер 20
         }
         sorted(func_max, population_max, LEN);
         sorted(func_min, population_min, LEN, -1);
-    } while (iter < 1000);
-    cout << iter << " Iterations" << endl;
+    } while (iter < iter_max && check_stop(func_max, func_min, LEN));
     
+    cout << iter << " Iterations" << endl;
     cout << "Максимум: " << endl;
     cout << "f(x, y, z) = " << func_max[0] << endl;
     cout << population_max[0][0] << " " << population_max[0][1] << " " << population_max[0][2] << endl;
